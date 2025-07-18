55397bdaea2684e7591f64dd87f9c3847e14c2e9
gurgelricardo <ra24540901-5@aluno.unicesumar.edu.br>
2025-07-18 19:51:31 -0300

    outras correções + htmls e style.css

--- users/models.py
+++ users/models.py
@@ -5,4 +5,4 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     bio = models.TextField(blank=True, null=True, verbose_name="Biografia")
     connections = models.ManyToManyField("self", blank=True, symmetrical=True)
-    birth_date = models.DateField(blank=True, null=False)
+    birth_date = models.DateField(blank=True, null=True)
